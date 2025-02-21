5b2d64804670c405059d6df20ba7185159d41d1c
ManOAction <7423159+ManOAction@users.noreply.github.com>
2025-02-20 17:00:03 -0700

    Building Motor Test

--- tests_hardware/motors.py
+++ tests_hardware/motors.py
@@ -6,7 +6,7 @@ Working current: 1.2A
 
 """
 
-from gpiozero import LED, Motor
+from gpiozero import Motor
 from time import sleep
 
 
@@ -15,37 +15,70 @@ MotorSpeed = float(".3")
 RMotor = Motor(13, 6)
 LMotor = Motor(19, 26)
 
-print("Testing right motor....")
-RMotor.forward(MotorSpeed)
-print("Forward")
-sleep(2)
-RMotor.backward(MotorSpeed)
-print("Backward")
-sleep(2)
-RMotor.stop()
 
-print("Testing left motor....")
-LMotor.forward(MotorSpeed)
-print("Forward")
-sleep(2)
-LMotor.backward(MotorSpeed)
-print("Backward")
-sleep(2)
-LMotor.stop()
+def stop_all():
+    RMotor.stop()
+    LMotor.stop()
 
-print("Testing both motors....")
-RMotor.forward(MotorSpeed)
-LMotor.forward(MotorSpeed)
-print("Forward")
-sleep(2)
-RMotor.backward(MotorSpeed)
-LMotor.backward(MotorSpeed)
-print("Backward")
-sleep(2)
-RMotor.stop()
-LMotor.stop()
 
-print("Motors at rest...")
-sleep(5)
+def motor_test(motorName):
+    if motorName == "RMotor":
+        print("Testing right motor....")
+        RMotor.forward(MotorSpeed)
+        print("Forward")
+        sleep(2)
+        RMotor.backward(MotorSpeed)
+        print("Backward")
+        sleep(2)
+        RMotor.stop()
+    elif motorName == "LMotor":
+        print("Testing left motor....")
+        LMotor.forward(MotorSpeed)
+        print("Forward")
+        sleep(2)
+        LMotor.backward(MotorSpeed)
+        print("Backward")
+        sleep(2)
+        LMotor.stop()
+    elif motorName == "Both":
+        print("Testing both motors....")
+        RMotor.forward(MotorSpeed)
+        LMotor.forward(MotorSpeed)
+        print("Forward")
+        sleep(2)
+        RMotor.backward(MotorSpeed)
+        LMotor.backward(MotorSpeed)
+        print("Backward")
+        sleep(2)
+        RMotor.stop()
+        LMotor.stop()
 
-print("Testing complete")
+    stop_all()
+
+    return True
+
+
+def main():
+
+    stop_all()
+
+    options = [(1, "RMotor"), (2, "LMotor"), (3, "Both"), (4, "Exit")]
+
+    while True:
+        print("Choose the motor you want to test: ")
+        for option in options:
+            print(f"{option[0]}. {option[1]}")
+        motorName = input("Enter Option Number: ")
+        motorName = int(motorName)
+        motorName = options[motorName - 1][1]
+
+        if motorName == "Exit":
+            break
+
+        motor_test(motorName)
+
+    print("Exiting....")
+
+
+if __name__ == "__main__":
+    main()
